f536ad3e409099d6a2bc8a8323efbda5d250d945
moenessboui <76037150+moenessboui@users.noreply.github.com>
2021-03-03 10:06:31 +0100

    added filters

--- src/components/productsOverviewComp/productsOverview.tsx
+++ src/components/productsOverviewComp/productsOverview.tsx
@@ -1,21 +1,55 @@
-import React from 'react'; 
-import styles from "./productsOverview.module.css" 
-import { Product } from "../../models/product" 
-import App from '../../App'; 
+import React, { useState } from 'react';
+import styles from "./productsOverview.module.css"
+import { Product } from "../../models/product"
+import App from '../../App';
 import ProductItem from '../productItemComp/productItemComp';
 
- 
+
 interface ProductsOverviewProps {
-    _productsOverviewProp: Product[]
+    _productsProp: Product[]
 }
 
-const ProductsOverview: React.FC<ProductsOverviewProps> = ({ _productsOverviewProp }) => {
+const ProductsOverview: React.FC<ProductsOverviewProps> = ({ _productsProp }) => {
+   
+    const [selectedFilter,setSelectedFilter]= useState<String>('none')
+
+    function filterProducts(arrayProducts: Product[]){
+        
+        //if no filter is selected, we display all products
+        if (selectedFilter === 'none') {
+            return arrayProducts
+        }
+        else{
+            let filteredProducts: Product[] = []
+            //we display only products that have category === selectedFilter variable
+            arrayProducts.forEach(p=> {
+                if(selectedFilter === p.category){
+                    filteredProducts.push(p)
+                }
+            })
+            return filteredProducts
+        }
+    
+    }
+
+    let displayedProducts = filterProducts(_productsProp)
+
     return (
-        <div>
+        <div style={{ borderRight: 'solid red 1px' }}>
+            <div className={styles.CategoryFiltersContainer}>
+            <button onClick={()=> setSelectedFilter('none')}> reset </button>
+                <button onClick={()=> setSelectedFilter('Cleaning')}> Cleaning </button>
+                <button onClick={()=> setSelectedFilter('Food')}> Food </button>
+                <button onClick={()=> setSelectedFilter('Sports')}> Sports </button>
+            </div>
+            <div>
+                Selected Tag: {selectedFilter}
+            </div>
             <h1> Products Overview </h1>
+
             <div className={styles.ProductsOverviewContainer}>
-                {_productsOverviewProp.map(p =>
-                   <ProductItem _productProp={p}/>
+                {displayedProducts.map(p =>
+                    <ProductItem _productProp={p} />
                 )}
             </div>
         </div>
