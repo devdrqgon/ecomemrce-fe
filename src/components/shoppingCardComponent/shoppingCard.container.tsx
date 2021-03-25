f7d1aeadfc0905a402e718097d626b1894d42634
devdrqgon <70473449+devdrqgon@users.noreply.github.com>
2021-03-25 10:15:10 +0100

    added checkout poc

--- src/components/shoppingCardComponent/shoppingCard.container.tsx
+++ src/components/shoppingCardComponent/shoppingCard.container.tsx
@@ -3,15 +3,21 @@ import { Product } from "../../models/product"
 import styles from "./shoppingCard.module.css"
 
 
-interface ShoppingCardProps {
-   
-}
 
-const ShoppingCardPage: React.FC<ShoppingCardProps> = ({}) => {
+const ShoppingCardPage: React.FC= ({}) => {
     const prodOne = new Product("sadoiuad","bnlabl",23,"sadad")
-    const prodTwo = new Product("sadoiuasd2ad","ztz",23,"sadad")
-    const prodThree = new Product("sadoiuaasdd","bnladabl",23,"sadad")
+    const prodTwo = new Product("sadoiuasd2ad","ztz",54,"sadad")
+    const prodThree = new Product("sadoiuaasdd","bnladabl",230,"sadad")
     const prodList = [prodOne,prodTwo, prodThree]
+    function calculateSum(_inputList: Product[]){
+        let sum = 0
+        prodList.forEach(p => {
+            sum = sum + p.price
+        });
+        return sum
+    }
+    let itemsCount = prodList.length
+    let totalprice = calculateSum(prodList)
     return (
         <div className={styles.shoppingCardPageContainer}>
             <div className={styles.shoppingCardContainer}>
@@ -43,7 +49,12 @@ const ShoppingCardPage: React.FC<ShoppingCardProps> = ({}) => {
                 </div>
             </div>
             <div className={styles.checkoutContainer}>
-                checkout
+                <div>
+                    Subtotal ({itemsCount}): {totalprice} euros
+                </div>
+                <div>
+                    <button> Checkout </button>
+                </div>
             </div>
             
         </div>
