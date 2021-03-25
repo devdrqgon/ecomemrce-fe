9f41562c53f5ee8b86a78af596b6c3017624edc8
devdrqgon <70473449+devdrqgon@users.noreply.github.com>
2021-03-25 17:23:56 +0100

    poc routing

--- src/components/layout/masterLayout.tsx
+++ src/components/layout/masterLayout.tsx
@@ -3,31 +3,41 @@ import styles from "./masterLayout.module.css"
 import React from 'react'
 import { Product } from '../../models/product';
 import ProductsPage from '../productsPage/productsPage';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ShoppingCardPage from '../shoppingCardComponent/shoppingCard.container';
+
+
 const { Header, Content } = Layout;
 
 
 const MasterLayout: React.FC = () => {
 
- const prodOne = new Product("sadoiuad","bnlabl",23,"sadad")
-     const prodTwo = new Product("sadoiuasd2ad","ztz",54,"sadad")
-     const prodThree = new Product("sadoiuaasdd","bnladabl",230,"sadad")
-     const prodList = [prodOne,prodTwo, prodThree]
+ 
 return(
-    <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="2">Products</Menu.Item>
-        <Menu.Item key="3">Shopping Cart</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>    
-      <div className={styles.siteLayoutContent}>
-           <ProductsPage productsListProp={prodList}/>
-      </div>
-    </Content>
-    
+   <Router>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="2">
+              Products
+              <Link to="/products" />
+             
+            </Menu.Item>
+            <Menu.Item key="3">
+              Shopping Cart
+              <Link to="/shoppingcart" />
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>    
+          <div className={styles.siteLayoutContent}>
+            <Route exact path="/products" component={ProductsPage} />
+            <Route path="/shoppingcart" component={ShoppingCardPage} />       
+          </div>
+        </Content>
   </Layout>
+   </Router>
 )
 }
 
